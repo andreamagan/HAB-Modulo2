5a66982c474a982b9712f6db9fb18cdac6f18954
Andrea <andrea.magan@outlook.com>
2019-01-25 21:34:32 +0100

    Ejercicios día 5

--- dia5/app.js
+++ dia5/app.js
@@ -3,4 +3,60 @@
  * practicar Javascript!
  */
 
-console.log("Probando, probando, 1, 2, 3!");
+console.log("Probando, probando, 1, 2, 3!";
+
+//Ejercico 1
+function ask (question, yes, no)  {
+    if (confirm (question)) {yes()}
+    else {no();}
+}
+
+ask{
+    "Do yo agree?",
+    () => alert ("You agreed."),
+    () => alert ("You canceled the execution.")
+};
+
+
+//Ejercicio 2
+let user = {};
+user.name = "Jonh";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+//Ejercicio 3
+
+function isEmpty(obj){
+    for (let key in obj){ // recorre las probpiedades del objeto
+        return false;
+    }
+    return true;
+}
+
+//Ejercicio 4 
+const user = {
+    name: "John"
+  };
+  // does it work?
+  user.name = "Pete";// Si porque no cambiar el user, lo que cambias es la propiedad
+
+  //Ejercicio 5
+
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+
+let sum = 0;
+
+for (let key in salaries) {
+    sum += salaries.[key];
+}
+
+alert(sum);
+
+
+
+
--- dia5/appextra.js
+++ dia5/appextra.js
@@ -4,3 +4,22 @@
  */
 
 console.log("Probando, probando, 1, 2, 3!");
+
+//
+let unObj{}
+id.unObj = "01";
+name.unObj = "miNombre";
+
+
+if (name.length < 3) {
+    return;
+} 
+else{uperCase();} 
+
+
+
+function uperCase(name.unObj){
+
+}
+
+alert (...);
